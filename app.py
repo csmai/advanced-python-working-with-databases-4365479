43d441d5fe02d71b40cb72be9a8312518a3779ea
csmai <47118605+csmai@users.noreply.github.com>
2023-06-27 14:44:10 +0000

    feladat teljesítve

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import sqlite3
 
 connection = sqlite3.connect('movies.db')
 cursor = connection.cursor()
+release_year = (2012,)
 
+cursor.execute('SELECT * FROM Movies WHERE year=?', release_year)
+print(cursor.fetchall())
 
 
 connection.commit()
