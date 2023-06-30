b31a4bee777761f0a07b3400be47d8d9c724778d
csmai <47118605+csmai@users.noreply.github.com>
2023-06-30 14:48:29 +0000

    sqlalchemy expersison kuzdes

--- sqlalchemy-workspace/database.py
+++ sqlalchemy-workspace/database.py
@@ -1,3 +1,21 @@
 import sqlalchemy
 
-engine = sqlalchemy.create_engine('sqlite:///movies.db', echo=True)
+engine = sqlalchemy.create_engine("sqlite:///movies.db", echo=True)
+
+metadata = sqlalchemy.MetaData()
+
+table_object = sqlalchemy.Table(
+    "Movies",
+    metadata,
+    sqlalchemy.Column("title", sqlalchemy.Text),
+    sqlalchemy.Column("director", sqlalchemy.Text),
+    sqlalchemy.Column("year", sqlalchemy.Integer),
+)
+
+# Instantiate all the Tables in the database
+metadata.create_all(engine)
+
+# Connect to the database
+with engine.connect() as conn:
+    for row in conn.execute(sqlalchemy.select(table_object)):
+        print(row)
