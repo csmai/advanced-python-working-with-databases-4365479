96fd36f0a6c8da9c6723c203063010038d33e1c2
csmai <maicsilla@gmail.com>
2023-07-01 10:02:50 +0200

    javitas, hogy fusson a kod + autoformat

--- mysql-workspace/database.py
+++ mysql-workspace/database.py
@@ -1,30 +1,32 @@
 import mysql.connector as mysql
 
+
 def connect(db_name):
-	try:
-		return mysql.connect(host="localhost",
-							 database=db_name,
-							 user='root',
-							 password="password")
-	except Error as e:
-		print(e)
+    try:
+        return mysql.connect(
+            host="localhost", database=db_name, user="root", password="..."
+        )
+    except mysql.Error as e:
+        print(e)
+
 
 def add_project(cursor, project_title, project_description, tasks):
-	pass
+    pass
 
-if __name__ == '__main__':
-	db = connect("projects")
 
-	cursor = db.cursor()
+if __name__ == "__main__":
+    db = connect("projects")
 
-	tasks = ["Clean bathroom", "Clean kitchen", "Clean living room"]
-	add_project(cursor, "Clean house", "Clean house by room", tasks)
-	db.commit()
+    cursor = db.cursor()
 
-	cursor.execute("SELECT * FROM projects")
-	project_records = cursor.fetchall()
-	print(project_records)
+    tasks = ["Clean bathroom", "Clean kitchen", "Clean living room"]
+    add_project(cursor, "Clean house", "Clean house by room", tasks)
+    db.commit()
 
-	cursor.execute("SELECT * FROM tasks")
-	tasks_records = cursor.fetchall()
-	print(tasks_records)
+    cursor.execute("SELECT * FROM projects")
+    project_records = cursor.fetchall()
+    print(project_records)
+
+    cursor.execute("SELECT * FROM tasks")
+    tasks_records = cursor.fetchall()
+    print(tasks_records)
